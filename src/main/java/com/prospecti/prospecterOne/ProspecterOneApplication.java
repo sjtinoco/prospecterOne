086f18566edbfeb522948b6e641ae97aa33c8f3d
sjtinoco <sjtinoco@yahoo.com.br>
2024-09-16 22:14:30 -0300

    Project created

---
+++ src/main/java/com/prospecti/prospecterOne/ProspecterOneApplication.java
@@ -0,0 +1,13 @@
+package com.prospecti.prospecterOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspecterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspecterOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prospecti/prospecterOne/ProspecterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prospecti.prospecterOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspecterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
