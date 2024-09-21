28a6f4bc666e3ee246d8fec784207437ed93a68d
sjtinoco <sjtinoco@yahoo.com.br>
2024-09-20 23:25:24 -0300

    Service layer, component registration

--- src/main/java/com/prospecti/prospecterOne/repositories/UserRepository.java
+++ src/main/java/com/prospecti/prospecterOne/repositories/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prospecti.prospecterOne.entities.User;
 
+
+// não precisa colocar @Rpository pois herda de JpaRepository que já é um componente spring
 public interface UserRepository extends JpaRepository<User, Long>  {
-	
-	
 }
--- src/main/java/com/prospecti/prospecterOne/resources/UserResource.java
+++ src/main/java/com/prospecti/prospecterOne/resources/UserResource.java
@@ -1,19 +1,42 @@
 package com.prospecti.prospecterOne.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.prospecti.prospecterOne.entities.User;
+import com.prospecti.prospecterOne.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResource {
+public class UserResource {//Controlador rest
 
+//	@GetMapping
+//	public ResponseEntity<User> findALL(){
+//		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+//		return ResponseEntity.ok().body(u);
+//	}
+	
+	@Autowired // só pode injetar o que for componente do spring
+	           // uma classe criada pelo programador pode ser componente
+			   // com as anotações @Component, @Repository, @Service
+	private UserService service; // a classe UserService deve receber uma anotação de componente
+	
 	@GetMapping
-	public ResponseEntity<User> findALL(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findALL(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
