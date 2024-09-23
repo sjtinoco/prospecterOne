bec2237ee206cf7a23957d8a62704752095d4e7e
sjtinoco <sjtinoco@yahoo.com.br>
2024-09-22 21:50:48 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/prospecti/prospecterOne/entities/Category.java
+++ src/main/java/com/prospecti/prospecterOne/entities/Category.java
@@ -4,10 +4,13 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -20,7 +23,9 @@ public class Category {
 	private Long id;
 	private String name;
 	
-	@Transient // para o jpa ignorar
+	//@Transient // para o jpa ignorar
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories") // categories de "private Set<Category> categories = new HashSet<>();" em Product
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
