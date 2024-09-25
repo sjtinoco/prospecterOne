ed0b80b1c08ee5dd25b0cace9e506e7b7051689c
sjtinoco <sjtinoco@yahoo.com.br>
2024-09-24 22:17:37 -0300

    Exception handling - findById - delete - update

---
+++ src/main/java/com/prospecti/prospecterOne/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.prospecti.prospecterOne.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. id " + id);
+	}
+}
